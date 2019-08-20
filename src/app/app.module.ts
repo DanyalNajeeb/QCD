302f8ad01ff40d952fdc850e9458dd0f797c5883
Danyal Najeeb <dany.najeeb1995@gmail.com>
2019-08-20 23:51:17 +0500

    Login and homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule , Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { flush } from '@angular/core/testing';
+
+const appRoutes:Routes=[
+  {path:'home' , component:HomeComponent},
+  { path:'',
+    redirectTo:'/home',
+    pathMatch:'full'
+  },
+  {path:'loginPage', component:LoginComponent}
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    HomeComponent,
+    
+    NavbarComponent,
+    
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,{
+        enableTracing:true
+      }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
